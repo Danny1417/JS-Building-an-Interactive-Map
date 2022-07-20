cf1b9b92a281d918eca2657c70e62e2c62c093c5
danny1417 <mr.mejia1417@gmail.com>
2022-07-20 19:11:01 -0400

    added changes

--- public/app.js
+++ public/app.js
@@ -1,19 +1,37 @@
 // create map
+// Create map:                                                       
+const myMap = L.map('map', {
+    center: [48.868672, 2.342130],
+    zoom: 15,
+});
 
 
 // add openstreetmap tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(myMap);
 
 
-// create and main add geolocation marker
+// Create and add a geolocation marker:
+const marker = L.marker([48.87007, 2.346453])
+marker.addTo(myMap).bindPopup('<p1><b>The Hoxton, Paris</b></p1>').openPopup()
 
 
 // draw the 2nd arrondissement
 
+const latlngs = [
+    [48.863368120198004, 2.3509079846928516],
+    [48.86933262048345, 2.3542531602919805],
+    [48.87199261164275, 2.3400569901592183],
+    [48.86993336274516, 2.3280142476578813],
+    [48.86834104280146, 2.330308418109664],
+];
+const polygon = L.polygon(latlngs, { color: "blue", fillColor: "none", }).addTo(myMap)
+
+
+
 
 // create red pin marker
 
-
 // metro station markers
-
-
-
