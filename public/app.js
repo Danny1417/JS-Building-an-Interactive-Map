cff786a966bfa929e37e4340f392ff6ebe85cf7b
danny1417 <mr.mejia1417@gmail.com>
2022-07-22 08:35:54 -0400

    added red pin

--- public/app.js
+++ public/app.js
@@ -28,10 +28,20 @@ const latlngs = [
     [48.86834104280146, 2.330308418109664],
 ];
 const polygon = L.polygon(latlngs, { color: "blue", fillColor: "none", }).addTo(myMap)
+    // create red pin marker
+const redPin = L.icon({
+    iconUrl: "/public/assets/red-pin.png",
+    iconSize: [38, 38],
+    iconAnchor: [19, 38],
 
 
+});
+// Metro station markers:
+const rS = L.marker([48.866200610611926, 2.352236247419453], { icon: redPin }).bindPopup('RÃ©aumur-SÃ©bastopol')
+const sSD = L.marker([48.869531786321566, 2.3528590208055196], { icon: redPin }).bindPopup('Strasbourg-Saint-Denis')
+const sentier = L.marker([48.8673721067762, 2.347107922912739], { icon: redPin }).bindPopup('Sentier')
+const bourse = L.marker([48.86868503971672, 2.3412285142058167], { icon: redPin }).bindPopup('Bourse')
+const qS = L.marker([48.869560129483226, 2.3358638645569543], { icon: redPin }).bindPopup('Quatre Septembre')
+const gB = L.marker([48.871282159004856, 2.3434818588892714], { icon: redPin }).bindPopup('Grands Boulevards')
 
-
-// create red pin marker
-
-// metro station markers
+const stations = L.layerGroup([rS, sSD, sentier, bourse, qS, gB]).addTo(myMap)
